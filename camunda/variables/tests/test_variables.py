4f2d164c8e3a2bd32c2aeb4fc76a6e73c733e45f
Alessio Gerace <alessio.gerace@gmail.com>
2023-02-15 08:57:00 +0100

    [UPD] 4.3

--- camunda/variables/tests/test_variables.py
+++ camunda/variables/tests/test_variables.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from camunda.variables.variables import Variables
-
+import json
 
 class VariablesTest(TestCase):
 
@@ -34,29 +34,33 @@ class VariablesTest(TestCase):
     def test_format_returns_dict_with_value_when_nested_dict(self):
         var1_raw = {"var2": 1, "var3": "test"}
         variables = {"var1": var1_raw}
-        self.assertDictEqual({"var1": {"value": var1_raw}}, Variables.format(variables))
+        self.assertDictEqual(
+            {"var1": {"type": "Json", "value": json.dumps(var1_raw)}},
+            Variables.format(variables))
 
     def test_format_returns_formatted_variables_when_variables_present(self):
         variables = {"var1": 1, "var2": True, "var3": "string"}
         formatted_vars = Variables.format(variables)
         self.assertDictEqual({"var1": {"type": "Integer", "value": 1},
                               "var2": {"type": "Boolean", "value": True},
-                              "var3": {"type": "String", "value": "string"}}, formatted_vars)
+                              "var3": {"type": "String", "value": "string"}},
+                             formatted_vars)
 
     def test_format_returns_formatted_variables_keeps_already_formatted(self):
-        variables = {"var1": 1, "var2": True, "var3": "string", "var4": {"value": 1}}
+        variables = {"var1": 1, "var2": True, "var3": "string",
+                     "var4": {"value": 1}}
         formatted_vars = Variables.format(variables)
         print(formatted_vars)
         self.assertDictEqual({'var1': {'type': 'Integer', 'value': 1},
                               'var2': {'type': 'Boolean', 'value': True},
                               'var3': {'type': 'String', 'value': 'string'},
-                              'var4': {'type': 'Json', 'value': '{"value": 1}'}},
+                              'var4': {'type': 'Json',
+                                       'value': '{"value": 1}'}},
                              formatted_vars)
 
     def test_to_dict_returns_variables_as_dict(self):
         variables = Variables({"var1": {"type": "Integer", "value": 1},
                                "var2": {"type": "Boolean", "value": True},
                                "var3": {"type": "String", "value": "string"}})
-        self.assertDictEqual({"var1": 1, "var2": True, "var3": "string"}, variables.to_dict())
-
-
+        self.assertDictEqual({"var1": 1, "var2": True, "var3": "string"},
+                             variables.to_dict())
